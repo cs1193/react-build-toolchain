29f90fb3a70ebc8c7642b61da7d329b990536583
Chandresh Rajkumar Manonmani <cs1193@gmail.com>
2017-11-08 00:16:55 +0530

    Separate Modules

--- default-webpack.dev.config.js
+++ default-webpack.dev.config.js
@@ -1,10 +1,5 @@
-const path = require('path');
-const PACKAGE = require('./package.json');
-
-const banner = PACKAGE.name + ' - ' + PACKAGE.version + ' | (c) ' + new Date().getFullYear() + ', ' + PACKAGE.author + ' | ' + PACKAGE.license + ' | ' + PACKAGE.homepage;
-
 /**
- * A function thaat returns a default webpack development configuration for local development.
+ * A function that returns a default webpack development configuration for local development.
  * @param       {Object} webpack           A reference to the webpack module.
  * @param       {Object} HtmlWebpackPlugin A reference to the html-webpack-plugin module.
  * @param       {Object} ExtractTextPlugin A reference to the extract-text-webpack-plugin module.
@@ -14,10 +9,7 @@ function WebpackDevelopmentConfiguration (webpack, HtmlWebpackPlugin, ExtractTex
     cache: true,
     watch: true,
     context: __dirname,
-    entry: {
-      scripts: ['react', './example/example.js'],
-      styles: ['./example/example.scss']
-    }
+    entry: entry
   };
 }
 
---
+++ webpack/entry.js
@@ -0,0 +1,4 @@
+module.exports = {
+  scripts: ['react', './source/index.js'],
+  styles: ['./source/index.scss']
+};
